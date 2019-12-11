f0f91a7ea17e6fbc08b94876a6a42dd716793ff5
Nydragon00 <56591727+Nydragon00@users.noreply.github.com>
2019-12-11 17:28:33 +0100

    problems with the weather report

--- tools/weather.py
+++ tools/weather.py
@@ -13,10 +13,41 @@ def get_weather(location, api):
     r = requests.get(url)
     return r.json()
 
+def constuct_report(data):
+    main = list(data)
+
+    main_index = [1, 3, 5, 8]
+    weather_index = [1]
+    main_index = [0, 1, 2, 3, 5]
+    wind_index = [0]
+    sys_index = [2, 7]
+
+    main_l = []
+
+    for i in main_index:
+        main_l.append(main[i])
+
+    for i in weather_index:
+        weather_l = list(data[main_l[0]])
+
+    for i in main_l:
+        print(i)
+        # for x in weather_l:
+        #     print()
+            #print(data[i][x])
+    # print(data[main_l[0]][weather_l[i]])
+
 def weather(*args):
     if None in args:
         return 'You have to chose a city.'
     api = get_api_key()
     city = args[0]
     data = get_weather(city, api)
-    return data
+
+    constuct_report(data)
+
+    # print(list(data[main[1]]))
+    #
+    # weather_report = '\nToday there are/is {}.'.format(data[1][1])
+
+    return 1
